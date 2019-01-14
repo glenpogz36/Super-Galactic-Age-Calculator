8d303444d2deb4ebd373d7769a04202fb6ccb32e
glen <glen_sale@yahoo.com>
2019-01-13 16:37:08 -0800

    it will add more planets using back-end logic

--- src/galaxy.js
+++ src/galaxy.js
@@ -3,7 +3,11 @@ const planetYears = {
     mercuryAge: 0.24,
     venusAge: 0.62,
     marsAge: 1.88,
-    jupiterAge: 11.86
+    jupiterAge: 11.86,
+    saturnAge: 29.45,
+    urnanusAge: 84.01,
+    neptuneAge: 164.79,
+    plutoAge: 247.92,
 };
 
 // Life Expectancy 
@@ -11,7 +15,11 @@ const lifeExpectancy = {
     marsLimit: 3,
     mercuryLimit: 7,
     venusLimit: 9,
-    jupiterLimit: 12
+    jupiterLimit: 12,
+    saturnLimit: 15,
+    urnanusLimit: 18,
+    neptuneLimit: 21,
+    plutoLimit: 25,
 };
 
 // Main Something
@@ -56,11 +64,47 @@ export class galaxy {
     JupiterAgeLimit() {
         return Math.floor((this.age * lifeExpectancy.jupiterLimit / planetYears.jupiterAge));
     }
+    //   Saturn Planet
+    ConvertSaturnAge() {
+        return Math.floor((this.age / planetYears.saturnAge));
+    }
+
+    SaturnAgeLimit() {
+        return Math.floor((this.age * lifeExpectancy.saturnLimit / planetYears.saturnAge));
+    }
+
+    //   Uranus Planet
+    ConvertUranusAge() {
+        return Math.floor((this.age / planetYears.uranusAge));
+    }
+
+    UranusAgeLimit() {
+        return Math.floor((this.age * lifeExpectancy.uranusLimit / planetYears.uranusAge));
+    }
+
+    //   Neptune Planet
+    ConvertNeptuneAge() {
+        return Math.floor((this.age / planetYears.neptuneAge));
+    }
+
+    NeptuneAgeLimit() {
+        return Math.floor((this.age * lifeExpectancy.neptunerLimit / planetYears.neptuneAge));
+    }
+
+    //   Pluto Planet
+    ConvertPlutoAge() {
+        return Math.floor((this.age / planetYears.plutoAge));
+    }
+
+    PlutoAgeLimit() {
+        return Math.floor((this.age * lifeExpectancy.plutoLimit / planetYears.plutoAge));
+    }
+
 
     //  Years Left
     PlanetYearsLeft() {
         var earthAge = 75;
-        if ((this.MercuryAgeLimit() || this.MarsAgeLimit() || this.JupiterAgeLimit() || this.VenusAgeLimit() <= earthAge))
+        if ((this.MercuryAgeLimit() || this.MarsAgeLimit() || this.JupiterAgeLimit() || this.VenusAgeLimit() || this.SaturnAgeLimit() || this.UranusAgeLimit() || this.NeptuneAgeLimit() || this.PlutoAgeLimit() <= earthAge))
             return true;
         else {
             return false;
